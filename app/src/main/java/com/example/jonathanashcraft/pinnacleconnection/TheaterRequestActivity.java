cd036cb052d635687e99be7b1fa06f7cb291afba
hibroseph <28901411+hibroseph@users.noreply.github.com>
2018-02-28 13:28:36 -0700

    added layouts for maintenanceRequest and theaterRequest

---
+++ app/src/main/java/com/example/jonathanashcraft/pinnacleconnection/TheaterRequestActivity.java
@@ -0,0 +1,18 @@
+package com.example.jonathanashcraft.pinnacleconnection;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class TheaterRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theater_request);
+
+    }
+}
