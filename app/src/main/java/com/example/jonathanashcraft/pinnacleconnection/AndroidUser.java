92ad79148f180867269cf2984e6250dca6e98ec9
hibroseph <iwillridgley@gmail.com>
2018-03-20 23:09:13 -0600

    Added a dialog box with a custom layout for submitting a maintenance request

--- app/src/main/java/com/example/jonathanashcraft/pinnacleconnection/AndroidUser.java
+++ app/src/main/java/com/example/jonathanashcraft/pinnacleconnection/AndroidUser.java
@@ -1,6 +1,7 @@
 package com.example.jonathanashcraft.pinnacleconnection;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -56,6 +57,7 @@ public class AndroidUser {
 
                 // Set the member variable user
                 user.setUser(mTempUser);
+                Log.d(TAG, "User successfully and  completely loaded");
             }
 
             @Override
